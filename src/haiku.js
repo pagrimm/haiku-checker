841eda7335446b55ff48bd129845150f81b26af8
Peter Grimm <pagrimm@gmail.com>
2020-06-25 09:54:10 -0700

    add passing test for subtracting silent vowels
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- src/haiku.js
+++ src/haiku.js
@@ -9,7 +9,24 @@ export function countVowels(word) {
   return count;
 }
 
+function isCharVowel(char) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.includes(char)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export function countSyllables (word) {
   let syllables = countVowels(word);
+  if (word[word.length -1] === "e") {
+    syllables --;
+  }
+  for (let i = 0; i < word.length; i ++) {
+    if (isCharVowel(word[i]) && isCharVowel(word[i + 1])) {
+      syllables --;
+    }
+  }
   return syllables;
 }
